111c0510c6ee101de1627b2d4697cb70fed3e7c0
Andrew <andrewkim102@ucsb.edu>
2025-04-27 04:33:22 -0700

    AK - Add JavaScript Fixtures for RecReq Table

---
+++ frontend/src/fixtures/RecommendationRequestFixtures.js
@@ -0,0 +1,42 @@
+const RecommendationRequestFixtures = {
+    oneRecommendationRequest: {
+      id: 1,
+      requesterEmail: "cgaucho@ucsb.edu",
+      professorEmail: "phtcon@ucsb.edu",
+      explanation: "BS/MS program",
+      dateRequested: "2022-04-20T00:00:00.018Z",
+      dateNeeded: "2022-05-01T00:00:00.018Z",
+      done: (false),
+    },
+    threeRecommendationRequests: [
+      {
+        id: 2,
+        requesterEmail: "ldelplaysa@ucsb.edu",
+        professorEmail: "richert@ucsb.edu",
+        explanation: "PhD CS Stanford",
+        dateRequested: "2022-04-20T00:00:00.018Z",
+        dateNeeded: "2022-11-11T00:00:00.018Z",
+        done: (false),
+      },
+      {
+        id: 3,
+        requesterEmail: "rubenking123@ucsb.edu",
+        professorEmail: "kingbob321@ucsb.edu",
+        explanation: "PhD CS MIT",
+        dateRequested: "2022-10-29T00:00:00.018Z",
+        dateNeeded: "2023-01-21T00:00:00.018Z",
+        done: (true),
+      },
+      {
+        id: 4,
+        requesterEmail: "alu@ucsb.edu",
+        professorEmail: "csguyprofessor@ucsb.edu",
+        explanation: "PhD CE Cal Tech",
+        dateRequested: "2020-11-19T00:00:00.018Z",
+        dateNeeded: "2024-04-06T00:00:00.018Z",
+        done: (false),
+      },
+    ],
+  };
+  
+  export { RecommendationRequestFixtures };
