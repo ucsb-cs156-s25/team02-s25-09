385cb11183f1c7f2fb24c6859b7071c43e1466a0
Ruben Alvarez <rubenntrecee@icloud.com>
2025-05-01 00:44:20 -0700

    ra - added fixtures for UCSBDiningCommonsMenuItem

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneItem: {
+      "id": 2,
+      "diningCommonsCode": "Portola",
+      "name": "Sushi",
+      "station": "Sushi Bar"
+  },
+  threeItems: [
+    {
+      "id": 2,
+      "diningCommonsCode": "Portola",
+      "name": "Sushi",
+      "station": "Sushi Bar"
+    },
+    {
+      "id": 3,
+      "diningCommonsCode": "Carillo",
+      "name": "Pizza",
+      "station": "Pizza Station"
+    },
+    {
+      "id": 4,
+      "diningCommonsCode": "De La Guerra",
+      "name": "Salad",
+      "station": "Salad Bar"
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
