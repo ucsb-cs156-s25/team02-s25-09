50b6b807b637878d4eeda45e01f62af13d10bfe2
Moiez Baqar <moiezybaqar@Moiezs-MacBook-Pro.local>
2025-05-02 01:21:36 -0700

    MB-ArticlesPlaceholder, reran format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,6 @@ import ArticlesIndexPage from "main/pages/Articles/ArticlesIndexPage";
 import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
 
-
 import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
@@ -74,8 +73,6 @@ function App() {
             />
           </>
         )}
-
-
 
         {hasRole(currentUser, "ROLE_USER") && (
           <>
