a61830cb0875204644fcb69ff0d524b25c42f794
Jsanchez021 <124843757+Jsanchez021@users.noreply.github.com>
2025-05-12 16:02:54 -0700

    Revert "Jz menuitemreview indexpage"

--- frontend/src/main/pages/MenuItemReviews/MenuItemReviewsIndexPage.js
+++ frontend/src/main/pages/MenuItemReviews/MenuItemReviewsIndexPage.js
@@ -1,49 +1,17 @@
-import React from "react";
-import { useBackend } from "main/utils/useBackend";
-
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import MenuItemReviewTable from "main/components/MenuItemReviews/MenuItemReviewTable";
-import { useCurrentUser, hasRole } from "main/utils/currentUser";
-import { Button } from "react-bootstrap";
 
 export default function MenuItemReviewsIndexPage() {
-  const currentUser = useCurrentUser();
-
-  const {
-    data: menuitemreviews,
-    error: _error,
-    status: _status,
-  } = useBackend(
-    // Stryker disable next-line all : don't test internal caching of React Query
-    ["/api/menuitemreview/all"],
-    { method: "GET", url: "/api/menuitemreview/all" },
-    // Stryker disable next-line all : don't test default value of empty list
-    [],
-  );
-
-  const createButton = () => {
-    if (hasRole(currentUser, "ROLE_ADMIN")) {
-      return (
-        <Button
-          variant="primary"
-          href="/menuitemreviews/create"
-          style={{ float: "right" }}
-        >
-          Create Menu Item Review
-        </Button>
-      );
-    }
-  };
-
+  // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
       <div className="pt-2">
-        {createButton()}
-        <h1>MenuItemReviews</h1>
-        <MenuItemReviewTable
-          menuitemreviews={menuitemreviews}
-          currentUser={currentUser}
-        />
+        <h1>Index page not yet implemented</h1>
+        <p>
+          <a href="/menuitemreviews/create">Create</a>
+        </p>
+        <p>
+          <a href="/menuitemreviews/edit/1">Edit</a>
+        </p>
       </div>
     </BasicLayout>
   );
