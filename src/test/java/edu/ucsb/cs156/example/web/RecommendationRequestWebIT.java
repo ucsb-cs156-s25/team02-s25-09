78e2826bf9002fe917bf929cd0208f6742e00150
Andrew <andrewkim102@ucsb.edu>
2025-05-12 20:52:11 -0700

    Finished e2e testing

--- src/test/java/edu/ucsb/cs156/example/web/RecommendationRequestWebIT.java
+++ src/test/java/edu/ucsb/cs156/example/web/RecommendationRequestWebIT.java
@@ -30,7 +30,6 @@ public class RecommendationRequestWebIT extends WebTestCase {
 
         page.getByText("Create Recommendation Request").click();
         assertThat(page.getByText("Create New Recommendation Request")).isVisible();
-
     
         page.getByLabel("Requester's Email").fill("tester1@ucsb.edu");
         page.getByLabel("Professor's Email").fill("tester2@ucsb.edu");
@@ -39,32 +38,35 @@ public class RecommendationRequestWebIT extends WebTestCase {
         page.getByLabel("Date Needed").fill("2022-05-01");
         page.getByLabel("Done").selectOption("True");
 
-
-
          page.getByTestId("RecommendationRequestForm-submit").click();
+        
 
-    //     assertThat(page.getByTestId("RestaurantTable-cell-row-0-col-description"))
-    //             .hasText("Build your own burrito chain");
+        assertThat(page.getByLabel("Explanation"))
+                .hasValue("Testing Explanation");
 
-    //     page.getByTestId("RestaurantTable-cell-row-0-col-Edit-button").click();
-    //     assertThat(page.getByText("Edit Restaurant")).isVisible();
-    //     page.getByTestId("RestaurantForm-description").fill("THE BEST");
-    //     page.getByTestId("RestaurantForm-submit").click();
+        page.getByTestId("RecommendationRequestTable-cell-row-0-col-Edit-button").click();
+        assertThat(page.getByText("Edit Recommendation Request")).isVisible();
+        page.getByLabel("Explanation").fill("Testing Explanation 2");
 
-    //     assertThat(page.getByTestId("RestaurantTable-cell-row-0-col-description")).hasText("THE BEST");
+        page.getByLabel("Date Requested").fill("2022-04-20");
+        page.getByLabel("Date Needed").fill("2022-05-01");
 
-    //     page.getByTestId("RestaurantTable-cell-row-0-col-Delete-button").click();
+        page.getByText("Update").click();
 
-    //     assertThat(page.getByTestId("RestaurantTable-cell-row-0-col-name")).not().isVisible();
+        assertThat(page.getByLabel("Explanation")).hasValue("Testing Explanation 2");
+
+        page.getByTestId("RecommendationRequestTable-cell-row-0-col-Delete-button").click();
+
+        assertThat(page.getByLabel("Explanation")).not().isVisible();
     }
 
-    // @Test
-    // public void regular_user_cannot_create_restaurant() throws Exception {
-    //     setupUser(false);
+    @Test
+    public void regular_user_cannot_create_RecommendationRequest() throws Exception {
+        setupUser(false);
 
-    //     page.getByText("Restaurants").click();
+        page.getByText("Recommendation Requests").click();
 
-    //     assertThat(page.getByText("Create Restaurant")).not().isVisible();
-    //     assertThat(page.getByTestId("RestaurantTable-cell-row-0-col-name")).not().isVisible();
-    // }
+        assertThat(page.getByText("Create Recommendation Request")).not().isVisible();
+        assertThat(page.getByTestId("RecommendationRequestTable-cell-row-0-col-name")).not().isVisible();
+    }
 }
